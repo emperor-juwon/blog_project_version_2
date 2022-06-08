8eb55581caac4940af33e678e30784b76030c623
emperor-juwon <fwangjuwon@gmail.com>
2022-06-09 02:13:49 +0900

    프로젝트 세팅(application.yml)

---
+++ src/main/java/site/metacoding/blog_project_version_2/BlogProjectVersion2Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.blog_project_version_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjectVersion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogProjectVersion2Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/blog_project_version_2/BlogProjectVersion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.blog_project_version_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogProjectVersion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
